4f640982c1ec31f01d474413e521849cf2f60b57
kirjh <71944013+kirjh@users.noreply.github.com>
2023-01-23 10:33:39 -0800

    Add proper icons for buttons

--- scripts/menu.js
+++ scripts/menu.js
@@ -82,7 +82,12 @@ const changeButtonColour = (alarmName) => {
 //  @id:      (string)     new id
 //  @icon:    (string)     new icon name
 const updateButton = (button, id, icon) => {
-  button.innerHTML = `<i class="material-icons">${icon}</i>`;
+  if (icon == "pause") {
+    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="svgbutton" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>`;
+  } else {
+    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="svgbutton" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>`;
+  }
+  
   button.id = id;
 
   return;
@@ -98,7 +103,7 @@ const togglePrimaryButton = async (button) => {
   // so it is separated from the switch statement and returns early.
   if (button.id == "init") {
     clearAlarm();
-    updateButton(button, "start", "play_arrow", "Start session");
+    updateButton(button, "start", "play_arrow");
     updateTime();
     return;
   }
@@ -142,7 +147,7 @@ const toggleStopButton = async (button, stopButton) => {
   chrome.storage.local.set({paused: false});
   button.classList.remove("alarmbuttonactive");
   stopButton.classList.remove("stopbuttonactive");
-  updateButton(button, "start", "play_arrow", "Start session");
+  updateButton(button, "start", "play_arrow");
   updateTime();
   return;
 }
@@ -291,6 +296,7 @@ const increaseAlarmLength = async () => {
 //  @pomodoro:  (number) number of pomodoros elapsed
 //  @alert:     (boolean) toggle creation of an alert
 const setCounter = (pomodoro, alert=false) => {
+  return; // TODO: remove
   const pomoCounter = document.getElementById("reset");
   pomoCounter.innerHTML = pomodoro;
   chrome.storage.session.set({pomocount: pomodoro});
